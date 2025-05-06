b28a0ec36038fafd00682c741a0d0828ebec257c
James Gayfer <10660608+jgayfer@users.noreply.github.com>
2025-05-06 14:07:10 -0700

    Don't cache voice channels

--- discord/guild.py
+++ discord/guild.py
@@ -470,6 +470,8 @@ class Guild(Hashable):
         self._from_data(data)
 
     def _add_channel(self, channel: GuildChannel, /) -> None:
+        if isinstance(channel, VoiceChannel):
+            return
         self._channels[channel.id] = channel
 
     def _remove_channel(self, channel: Snowflake, /) -> None:
